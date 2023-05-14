7c85429a8e0c34429597b051209f9e1d554577ad
TanyaEdyna <t.yamkova@gmail.com>
2023-05-14 10:19:20 +0300

    error checking

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -31,7 +31,7 @@ export class ContactForm extends Component {
             <input
                 className={css.input}
                 type="text"
-                name="name"
+                name="contactName"
                 placeholder="Name"
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,20 +2,20 @@ import PropTypes from 'prop-types';
 import css from "./ContactList.module.css";
 
 const ContactList = ({ contacts, filter, deleteContact }) => {
-    const filteredContacts = contacts.filter((contact) =>
-        contact.name.toLowerCase().includes(filter.toLowerCase())
-    )
-    return (
-        <ul className={css.contact_list}>
-            {filteredContacts.map(({ id, name, number }) => (
-                <li className={css.contact_item} key={id}>
-                    <p className={css.contact_name}>{name}: {number}</p>
-                    <button className={css.contact_btn_delete} type="button" onClick={() => deleteContact(id)}>Dlt</button>
-                </li>
-            )  
-            )}
-        </ul>
-    );
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={css.contact_list}>
+      {filteredContacts.map(({ id, name, number }) => (
+        <li className={css.contact_item} key={id}>
+          <p className={css.contact_name}>{name}: {number}</p>
+          <button className={css.contact_btn_delete} type="button" onClick={() => deleteContact(id)}>Dlt</button>
+        </li>
+      ))}
+    </ul>
+  );
 };
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
