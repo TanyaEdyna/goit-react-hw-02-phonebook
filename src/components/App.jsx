519282538bb4a68f137c99be0dbc44c32512d1f0
TanyaEdyna <t.yamkova@gmail.com>
2023-05-14 20:24:07 +0300

    add json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,13 @@ import React, { Component } from "react";
 import { ContactForm } from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
+import toContacts from "../components/contacts.json"
 import css from "./App.module.css";
 import { nanoid } from "nanoid";
 
-export class App extends Component {
+export class App extends Component { 
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
+    contacts: toContacts,
     filter: "",
   };
 
